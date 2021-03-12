1f1486768645812107eb900797f76ca3882cf193
Andrew Baluev <damico750@yandex.ru>
2021-03-12 20:52:38 +0300

    add dark theme

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const toggleTheme = document.querySelector("#toggle-theme");
+
+const html = document.documentElement;
+
+toggleTheme.addEventListener("click", function () {
+    if (html.hasAttribute("theme")) {
+        html.removeAttribute("theme");
+    } else {
+        html.setAttribute("theme", "dark");
+    }
+});
