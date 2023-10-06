ee1edf9c7620b58d7db053c77d5ce4cc1b102e49
Andrew <64975952+andrewbaluev@users.noreply.github.com>
2023-10-06 19:48:54 +0300

    Add ym target

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ const toggleTheme = document.querySelector("#toggle-theme");
 const html = document.documentElement;
 
 toggleTheme.addEventListener("click", function () {
+    ym(95178738, 'reachGoal', 'toggleTheme');
     if (html.hasAttribute("theme")) {
         html.removeAttribute("theme");
     } else {
