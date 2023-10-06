8391537b62a32fa4cb9a6dc7a2e09cb6e89d3004
Andrew <64975952+andrewbaluev@users.noreply.github.com>
2023-10-06 20:06:28 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,18 +1,4 @@
-const toggleTheme = document.querySelector("#toggle-theme");
-const html = document.documentElement;
-const savedTheme = sessionStorage.getItem("theme");
+import { toggleTheme } from "./theme.js";
+import { sendYandexMetricEvent } from "./analytics.js";
 
-if (savedTheme) {
-  html.setAttribute("theme", savedTheme);
-}
-
-toggleTheme.addEventListener("click", function () {
-    if (html.hasAttribute("theme")) {
-        html.removeAttribute("theme");
-        sessionStorage.setItem("theme", "light");
-    } else {
-        html.setAttribute("theme", "dark");
-        sessionStorage.setItem("theme", "dark");
-    }
-    ym(95178738, 'reachGoal', 'toggleTheme');
-});
+toggleThemeButton.addEventListener("click", toggleTheme);
