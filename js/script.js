ca199a4a7b02eaefe26b71828abb349778135694
Andrew <64975952+andrewbaluev@users.noreply.github.com>
2023-10-06 19:56:14 +0300

    save theme in sessionStorage

--- js/script.js
+++ js/script.js
@@ -1,12 +1,18 @@
 const toggleTheme = document.querySelector("#toggle-theme");
-
 const html = document.documentElement;
+const savedTheme = sessionStorage.getItem("theme");
+
+if (savedTheme) {
+  html.setAttribute("theme", savedTheme);
+}
 
 toggleTheme.addEventListener("click", function () {
-    ym(95178738, 'reachGoal', 'toggleTheme');
     if (html.hasAttribute("theme")) {
         html.removeAttribute("theme");
+        sessionStorage.setItem("theme", "light");
     } else {
         html.setAttribute("theme", "dark");
+        sessionStorage.setItem("theme", "dark");
     }
+    ym(95178738, 'reachGoal', 'toggleTheme');
 });
